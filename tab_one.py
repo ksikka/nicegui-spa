51382925ebd1407026d5fe093c3966b04b218c69
Karan Sikka <karanssikka@gmail.com>
2025-03-20 23:23:39 -0400

    initial commit

---
+++ tab_one.py
@@ -0,0 +1,40 @@
+import asyncio
+from typing import Any
+
+from nicegui import ui
+
+class TabOne:
+    container: Any = None
+    card: Any = None
+    i: int = 0
+    is_running = False
+    visibility = { "work_card": False}
+
+
+    def main(self):
+        self.container = ui.element('div')
+        with self.container:
+            ui.label('Content One').classes('text-2xl')
+            ui.button('do work', on_click=self.on_work_button_click)
+            c = ui.card()
+            c.bind_visibility_from(self.visibility, "work_card")
+            with c:
+                ui.label().bind_text_from(self, "work_button_text")
+
+    @property
+    def work_button_text(self):
+        return f"Work {self.i}"
+
+    async def on_work_button_click(self):
+        self.visibility["work_card"] = True
+        if self.is_running:
+            self.is_running = False
+            return
+        else:
+            self.is_running = True
+
+            for i in range(100):
+                if not self.is_running:
+                    break
+                self.i += 1
+                await asyncio.sleep(0.1)
